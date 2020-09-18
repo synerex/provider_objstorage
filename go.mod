fb9c4e3ec662c8ec6afffab8e9bc9d6607d7badf
Nobuo Kawaguchi <kawaguti@nagoya-u.jp>
2020-09-18 19:35:17 +0900

    objstorage beta is working

--- go.mod
+++ go.mod
@@ -4,10 +4,21 @@ go 1.14
 
 require (
 	github.com/golang/protobuf v1.4.2
+	github.com/google/uuid v1.1.2 // indirect
 	github.com/minio/minio-go/v7 v7.0.5
+	github.com/shirou/gopsutil v2.20.8+incompatible // indirect
 	github.com/synerex/proto_pcounter v0.0.6
 	github.com/synerex/proto_storage v0.0.2
 	github.com/synerex/synerex_api v0.4.2
 	github.com/synerex/synerex_proto v0.1.9
-	github.com/synerex/synerex_sxutil v0.4.12
+	github.com/synerex/synerex_sxutil v0.5.2
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
+	golang.org/x/net v0.0.0-20200904194848-62affa334b73 // indirect
+	golang.org/x/sys v0.0.0-20200917073148-efd3b9a0ff20 // indirect
+	google.golang.org/genproto v0.0.0-20200917134801-bb4cff56e0d0 // indirect
+	google.golang.org/grpc v1.32.0 // indirect
+	gopkg.in/ini.v1 v1.61.0 // indirect
+//	local.packages/synerex_sxutil v0.4.12
 )
+
+//replace local.packages/synerex_sxutil => ../../synerex_beta/sxutil
--- objstorage.go
+++ objstorage.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"flag"
 
@@ -15,24 +16,25 @@ import (
 
 	"log"
 	"sync"
-	"time"
 )
 
 var (
 	nodesrv         = flag.String("nodesrv", "127.0.0.1:9990", "Node ID Server")
 	local           = flag.String("local", "", "Local Synerex Server")
-	mu              sync.Mutex
-	version         = "0.01"
-	baseDir         = "store"
+	mu              *sync.Mutex
+	loop            *bool
+	currentPid      uint64 = 0
+	version                = "0.01"
+	baseDir                = "store"
 	dataDir         string
 	sxServerAddress string
-	mbusID          int64                   = -1 // storage MBus ID
+	mbusID          uint64                  = 0 // storage MBus ID
 	pc_client       *sxutil.SXServiceClient = nil
 	minioClient     *minio.Client
 )
 
 func init() {
-	endpoint := "127.0.0.1"
+	endpoint := "127.0.0.1:9000" // just use local minio for test.
 	accessKeyID := "minioadmin"
 	secretAccessKey := "minioadmin"
 	useSSL := false
@@ -46,41 +48,92 @@ func init() {
 		log.Fatalln(err)
 	}
 
-	log.Printf("Connected to Minio Client %#v\n", minioClient) // minioClient is now set up
-}
+	log.Printf("Connected to Minio Client %v, %#v\n", err, minioClient) // minioClient is now set up
 
-func reconnectClient(client *sxutil.SXServiceClient) {
-	mu.Lock()
-	if client.Client != nil {
-		client.Client = nil
-		log.Printf("Client reset \n")
+	buckets, berr := minioClient.ListBuckets(context.Background())
+	if berr != nil {
+		log.Println("Cant get buckets", berr)
+		return
 	}
-	mu.Unlock()
-	time.Sleep(5 * time.Second) // wait 5 seconds to reconnect
-	mu.Lock()
-	if client.Client == nil {
-		newClt := sxutil.GrpcConnectServer(sxServerAddress)
-		if newClt != nil {
-			log.Printf("Reconnect server [%s]\n", sxServerAddress)
-			client.Client = newClt
-		}
-	} else { // someone may connect!
-		log.Printf("Use reconnected server\n", sxServerAddress)
+	log.Printf("Bucket %d", len(buckets))
+	for n, bucket := range buckets {
+		log.Printf("%2d:%s,", n, bucket)
 	}
-	mu.Unlock()
+
 }
 
 func mbusCallback(clt *sxutil.SXServiceClient, mm *api.MbusMsg) {
 	log.Printf("Mbus message %#v", mm)
+
+	if sxutil.IDType(mm.TargetId) == clt.ClientID { // this msg is for me.
+		if mm.MsgInfo == "Store" {
+			record := &storage.Record{}
+			err := proto.Unmarshal(mm.Cdata.Entity, record)
+			if err == nil {
+				minioClient.PutObject(context.Background(), record.BucketName, record.ObjectName, bytes.NewBuffer(record.Record), int64(len(record.Record)), minio.PutObjectOptions{})
+				log.Printf("Store bucket %s, objname %s , len %d", record.BucketName, record.ObjectName, len(record.Record))
+			}
+		}
+	}
+
 }
 
-func supplyStorageCallback(clt *sxutil.SXServiceClient, dm *api.Demand) {
+func selectSupplyCB(clt *sxutil.SXServiceClient, dm *api.Demand) {
+	log.Printf("Current Supply Selected! %v", dm)
+	err := clt.Confirm(sxutil.IDType(dm.Id)) // send confirm to sender!
+	if err == nil {
+		log.Printf("Confirm success!")
+		go clt.SubscribeMbus(context.Background(), mbusCallback)
+	} else {
+		log.Printf("Confirm error %v", err)
+	}
+}
+
+func notifyDemandCB(clt *sxutil.SXServiceClient, dm *api.Demand) {
 	if dm.DemandName == "Storage" {
 		storageInfo := &storage.Storage{}
 		err := proto.Unmarshal(dm.Cdata.Entity, storageInfo)
+		log.Printf("Receive Storage Demand! %v", storageInfo)
+		if err == nil { /// lets start subscribe pcounter.
+			if storageInfo.Stype == storage.StorageType_TYPE_OBJSTORE && storageInfo.Dtype == storage.DataType_DATA_FILE {
+				log.Printf("Type OK")
+
+				cdata := api.Content{Entity: dm.Cdata.Entity}
+				spo := sxutil.SupplyOpts{
+					Target: dm.Id,
+					Name:   "Storage",
+					Cdata:  &cdata,
+				}
+				currentPid = clt.ProposeSupply(&spo)
+				log.Printf("Propose Supply %d, %v", currentPid, spo)
+			} else {
+				log.Printf("I can't support this storage type.")
+			}
+		} else {
+			log.Printf("Unmarshaling err %v", err)
+		}
+	}
+}
+
+/*
+func supplyStorageCallback(clt *sxutil.SXServiceClient, dm *api.Demand) {
+	//	log.Printf("Receive Callback: %v", dm)
+	if currentPid != 0 && currentPid == dm.TargetId { // select supply!!
+		log.Printf("Current Supply Selected! %v", dm)
+		err := clt.Confirm(sxutil.IDType(dm.Id)) // send confirm to sender!
+		if err == nil {
+			log.Printf("Confirm success!")
+			go clt.SubscribeMbus(context.Background(), mbusCallback)
+		} else {
+			log.Printf("Confirm error %v", err)
+		}
+	} else if dm.DemandName == "Storage" {
+		storageInfo := &storage.Storage{}
+		err := proto.Unmarshal(dm.Cdata.Entity, storageInfo)
+		log.Printf("Receive Storage Demand! %v", storageInfo)
 		if err == nil { /// lets start subscribe pcounter.
 			if dm.TargetId == 0 { // notifyDemand
-				log.Printf("Receive Notify Demand! %v", storageInfo)
+				//				log.Printf("Receive Notify Demand! %v", storageInfo)
 
 				if storageInfo.Stype == storage.StorageType_TYPE_OBJSTORE && storageInfo.Dtype == storage.DataType_DATA_FILE {
 					log.Printf("Type OK")
@@ -91,30 +144,55 @@ func supplyStorageCallback(clt *sxutil.SXServiceClient, dm *api.Demand) {
 						Name:   "Storage",
 						Cdata:  &cdata,
 					}
-					clt.ProposeSupply(&spo)
-					log.Printf("Propose Supply %v", spo)
+					currentPid = clt.ProposeSupply(&spo)
+					log.Printf("Propose Supply %d, %v", currentPid, spo)
 				}
-			} else if dm.TargetId == uint64(clt.ClientID) { // selected!
-				log.Printf("Select Demand! %v", storageInfo)
-				clt.Confirm(sxutil.IDType(dm.SenderId)) // send confirm to sender!
-				// now mbus id was set!
-				log.Printf("selected! send confirm %v", dm)
-
-				go clt.SubscribeMbus(context.Background(), mbusCallback)
 			} else {
 				log.Printf("Why this demand? %v", dm)
 			}
+		} else {
+			log.Printf("Unmarshaling err %v", err)
 		}
 	}
 
 }
+*/
 
-func subscribeStorageDemand(client *sxutil.SXServiceClient) {
-	ctx := context.Background() //
-	for {                       // make it continuously working..
-		client.SubscribeDemand(ctx, supplyStorageCallback)
-		log.Print("Error on subscribe")
-		reconnectClient(client)
+type myDemandHandler struct{}
+
+func (dh myDemandHandler) OnNotifyDemand(clt *sxutil.SXServiceClient, dm *api.Demand) *sxutil.SupplyOpts {
+	if dm.DemandName == "Storage" {
+		storageInfo := &storage.Storage{}
+		err := proto.Unmarshal(dm.Cdata.Entity, storageInfo)
+		if err == nil { /// lets start subscribe pcounter.
+			log.Printf("Receive Storage Demand! %v", storageInfo)
+			if storageInfo.Stype == storage.StorageType_TYPE_OBJSTORE && storageInfo.Dtype == storage.DataType_DATA_FILE {
+				log.Printf("Type OK")
+				cdata := api.Content{Entity: dm.Cdata.Entity}
+				spo := sxutil.SupplyOpts{
+					Name:  "Storage",
+					Cdata: &cdata,
+				}
+				return &spo // send "ProposeSupply" in handler
+			} else {
+				log.Printf("I can't support this storage type.")
+			}
+		} else {
+			log.Printf("Unmarshaling err %v", err)
+		}
+	}
+	return nil
+}
+func (dh myDemandHandler) OnSelectSupply(clt *sxutil.SXServiceClient, dm *api.Demand) bool {
+	log.Printf("OnSelectSupply called! %v", dm)
+	// always OK
+	return true
+}
+
+func (dh myDemandHandler) OnConfirmResponse(clt *sxutil.SXServiceClient, dm *api.Demand, err error) {
+	log.Printf("Confirm responsed! %v", err)
+	if err == nil {
+		go clt.SubscribeMbus(context.Background(), mbusCallback)
 	}
 }
 
@@ -148,7 +226,10 @@ func main() {
 
 	st_client := sxutil.NewSXServiceClient(client, pbase.STORAGE_SERVICE, "{Client:PCObjStore}")
 	log.Print("Subscribe Storage Demand")
-	go subscribeStorageDemand(st_client)
+
+	//	mu, loop = sxutil.SimpleSubscribeDemand(st_client, supplyStorageCallback)
+
+	mu, loop = sxutil.CombinedSubscribeDemand(st_client, notifyDemandCB, selectSupplyCB)
 
 	wg.Add(1)
 	//	go subscribePCounterSupply(pc_client)
